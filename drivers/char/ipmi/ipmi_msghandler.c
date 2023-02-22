301e1c012c4371c1334f984139b1721d9e301d17
Oliver Neukum <oneukum@suse.com>
2023-02-22 13:50:27 +0100

    ipmi: Move remove_work to dedicated workqueue (git-fixes).
    suse-commit: 7662fa06ebd9d11a0bece3f16f1c8e3f445edcdd

--- drivers/char/ipmi/ipmi_msghandler.c
+++ drivers/char/ipmi/ipmi_msghandler.c
@@ -221,6 +221,8 @@ struct ipmi_user {
 	struct work_struct remove_work;
 };
 
+struct workqueue_struct *remove_work_wq;
+
 static struct ipmi_user *acquire_ipmi_user(struct ipmi_user *user, int *index)
 	__acquires(user->release_barrier)
 {
@@ -1226,7 +1228,7 @@ static void free_user(struct kref *ref)
 	struct ipmi_user *user = container_of(ref, struct ipmi_user, refcount);
 
 	/* SRCU cleanup must happen in task context. */
-	schedule_work(&user->remove_work);
+	queue_work(remove_work_wq, &user->remove_work);
 }
 
 static void _ipmi_destroy_user(struct ipmi_user *user)
@@ -5341,10 +5343,17 @@ static int ipmi_init_msghandler(void)
 	mod_timer(&ipmi_timer, jiffies + IPMI_TIMEOUT_JIFFIES);
 
 	atomic_notifier_chain_register(&panic_notifier_list, &panic_block);
+	remove_work_wq = create_singlethread_workqueue("ipmi-msghandler-remove-wq");
+	if (!remove_work_wq) {
+		pr_err("unable to create ipmi-msghandler-remove-wq workqueue");
+		rv = -ENOMEM;
+		goto out;
+	}
 
 	initialized = 1;
 
-	return 0;
+out:
+	return rv;
 }
 
 static int __init ipmi_init_msghandler_mod(void)
@@ -5360,6 +5369,7 @@ static void __exit cleanup_ipmi(void)
 	if (!initialized)
 		return;
 
+	destroy_workqueue(remove_work_wq);
 	atomic_notifier_chain_unregister(&panic_notifier_list, &panic_block);
 
 	/*
