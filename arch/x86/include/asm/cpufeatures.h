f70f299165bed88b5ed4e048c6cd57a00ec20799
Nikolay Borisov <nik.borisov@suse.com>
2023-07-28 15:36:58 +0300

    x86/srso: Add IBPB_BRTYPE support (bsc#1213287, CVE-2023-20569).
    suse-commit: 52998d353296924137159446ef2c7738baeb091d

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -375,6 +375,8 @@
 #define X86_FEATURE_ARCH_CAPABILITIES	(18*32+29) /* IA32_ARCH_CAPABILITIES MSR (Intel) */
 #define X86_FEATURE_SPEC_CTRL_SSBD	(18*32+31) /* "" Speculative Store Bypass Disable */
 
+#define X86_FEATURE_IBPB_BRTYPE		(20*32+28) /* "" MSR_PRED_CMD[IBPB] flushes all branch type predictions */
+
 /*
  * BUG word(s)
  */
