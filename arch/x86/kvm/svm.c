ba60b9e02233955b611a203b1646e18cbaa161a2
Nikolay Borisov <nik.borisov@suse.com>
2023-08-04 12:59:34 +0300

    - Update config files. - Refresh patches.suse/x86-srso-add-srso_no-support.patch.
    Ensure SRSO is always built and also ensure that msr interception works
    correctly when writing to PRED_CMD msr with the SRSO_NO capability
    present.
    
    suse-commit: c88c60da197690df9638479c58f51b4697b8dff0

--- arch/x86/kvm/svm.c
+++ arch/x86/kvm/svm.c
@@ -4324,13 +4324,13 @@ static int svm_set_msr(struct kvm_vcpu *vcpu, struct msr_data *msr)
 		    !guest_has_pred_cmd_msr(vcpu))
 			return 1;
 
-		if (data & ~PRED_CMD_IBPB)
+		if (data & ~(PRED_CMD_IBPB | (boot_cpu_has(X86_FEATURE_SBPB) ? PRED_CMD_SBPB : 0)))
 			return 1;
 
 		if (!data)
 			break;
 
-		wrmsrl(MSR_IA32_PRED_CMD, PRED_CMD_IBPB);
+		wrmsrl(MSR_IA32_PRED_CMD, data);
 		if (is_guest_mode(vcpu))
 			break;
 		set_msr_interception(svm->msrpm, MSR_IA32_PRED_CMD, 0, 1);
