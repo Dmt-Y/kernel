4b89f4888960042905d32a55224a4f033f3f687c
Coly Li <colyli@suse.de>
2024-01-16 15:40:47 +0800

    libnvdimm: cover up changes in struct nvdimm_bus_descriptor (git-fixes).
    suse-commit: 27f581b25d6bf64d5ce65dd86fb90e37f3ae5c64

--- include/linux/libnvdimm.h
+++ include/linux/libnvdimm.h
@@ -85,8 +85,6 @@ struct nvdimm_bus_descriptor {
 	const struct attribute_group **attr_groups;
 	unsigned long bus_dsm_mask;
 	unsigned long cmd_mask;
-	unsigned long dimm_family_mask;
-	unsigned long bus_family_mask;
 	struct module *module;
 	char *provider_name;
 	struct device_node *of_node;
@@ -94,6 +92,10 @@ struct nvdimm_bus_descriptor {
 	int (*flush_probe)(struct nvdimm_bus_descriptor *nd_desc);
 	int (*clear_to_send)(struct nvdimm_bus_descriptor *nd_desc,
 			struct nvdimm *nvdimm, unsigned int cmd, void *data);
+#ifndef __GENKSYMS__
+	unsigned long dimm_family_mask;
+	unsigned long bus_family_mask;
+#endif
 };
 
 struct nd_cmd_desc {
