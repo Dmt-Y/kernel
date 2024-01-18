ca1bca161b0d5e9bb4a9c83afaa261c4ac473522
Denis Kirjanov <dkirjanov@suse.com>
2024-01-18 11:40:45 +0300

    preserve KABI for struct plat_stmmacenet_data (git-fixes).
    suse-commit: be0b5cc15ced99b5453ad45683df57783867cb44

--- include/linux/stmmac.h
+++ include/linux/stmmac.h
@@ -182,12 +182,14 @@ struct plat_stmmacenet_data {
 	struct clk *pclk;
 	struct clk *clk_ptp_ref;
 	unsigned int clk_ptp_rate;
-	unsigned int clk_ref_rate;
 	struct reset_control *stmmac_rst;
 	struct stmmac_axi *axi;
 	int has_gmac4;
 	bool tso_en;
 	int mac_port_sel_speed;
 	bool en_tx_lpi_clockgating;
+#ifndef __GENKSYMS__
+	unsigned int clk_ref_rate;
+#endif
 };
 #endif
