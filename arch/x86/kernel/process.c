3a252737c9d925633bed2fb73a80e93f0a29ea0e
Nikolay Borisov <nik.borisov@suse.com>
2024-01-22 18:18:37 +0200

    x86: Fix __get_wchan() for !STACKTRACE (git-fixes).
    suse-commit: 23a1a0e28a8dad0e48b2583e119f3eb85708d86f

--- arch/x86/kernel/process.c
+++ arch/x86/kernel/process.c
@@ -39,6 +39,7 @@
 #include <asm/desc.h>
 #include <asm/prctl.h>
 #include <asm/spec-ctrl.h>
+#include <asm/unwind.h>
 
 #include "process.h"
 
@@ -788,13 +789,23 @@ unsigned long arch_randomize_brk(struct mm_struct *mm)
  */
 unsigned long get_wchan(struct task_struct *p)
 {
-	unsigned long entry = 0;
+	struct unwind_state state;
+	unsigned long addr = 0;
 
 	if (p == current || p->state == TASK_RUNNING)
 		return 0;
 
-	stack_trace_save_tsk(p, &entry, 1, 0);
-	return entry;
+	for (unwind_start(&state, p, NULL, NULL); !unwind_done(&state);
+	     unwind_next_frame(&state)) {
+		addr = unwind_get_return_address(&state);
+		if (!addr)
+			break;
+		if (in_sched_functions(addr))
+			continue;
+		break;
+	}
+
+	return addr;
 }
 
 long do_arch_prctl_common(struct task_struct *task, int option,
