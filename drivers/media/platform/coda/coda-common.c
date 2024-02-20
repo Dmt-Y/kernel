03a6e1768d3f588e979d48ddb47332ee3b4ac785
Oliver Neukum <oneukum@suse.com>
2024-02-20 15:20:35 +0100

    - media: coda: wake up capture queue on encoder stop after output   streamoff (git-fixes). - Refresh   patches.suse/media-coda-fix-last-buffer-handling-in-V4L2_ENC_CMD_.patch.
    suse-commit: 4fba70df4ed3f14b768c66c592cd9a8c5ecc2cc2

--- drivers/media/platform/coda/coda-common.c
+++ drivers/media/platform/coda/coda-common.c
@@ -919,7 +919,7 @@ static int coda_encoder_cmd(struct file *file, void *fh,
 	flush_work(&ctx->pic_run_work);
 
 	/* If there is no buffer in flight, wake up */
-	if (ctx->qsequence == ctx->osequence) {
+	if (!ctx->streamon_out || ctx->qsequence == ctx->osequence) {
 		dst_vq = v4l2_m2m_get_vq(ctx->fh.m2m_ctx,
 					 V4L2_BUF_TYPE_VIDEO_CAPTURE);
 		dst_vq->last_buffer_dequeued = true;
