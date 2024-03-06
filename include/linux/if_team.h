d881322501370bf1c52e1ccd01764ab0d94e91a9
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2024-03-06 12:05:40 +0100

    kabi: team: Hide new member header_ops (bsc#1220870 CVE-2023-52574).
    suse-commit: 9fab77ad3c478ffc0cce72833b2ded83931c97ad

--- include/linux/if_team.h
+++ include/linux/if_team.h
@@ -178,8 +178,6 @@ struct team {
 	struct net_device *dev; /* associated netdevice */
 	struct team_pcpu_stats __percpu *pcpu_stats;
 
-	const struct header_ops *header_ops_cache;
-
 	struct mutex lock; /* used for overall locking, e.g. port lists write */
 
 	/*
@@ -212,6 +210,9 @@ struct team {
 		struct delayed_work dw;
 	} mcast_rejoin;
 	long mode_priv[TEAM_MODE_PRIV_LONGS];
+#ifndef __GENKSYMS__
+	const struct header_ops *header_ops_cache;
+#endif
 };
 
 static inline int team_dev_queue_xmit(struct team *team, struct team_port *port,
