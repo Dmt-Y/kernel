9706ea454d2d2633c61c8e6fc4022cd8afba9c95
Jan Kara <jack@suse.cz>
2024-03-20 18:17:24 +0100

    fs: Establish locking order for unrelated directories (bsc#1221044 CVE-2023-52591).
    suse-commit: c49cfde3c33d3175a6c13c69a65c39b4736c0371

--- fs/internal.h
+++ fs/internal.h
@@ -117,6 +117,8 @@ extern int vfs_open(const struct path *, struct file *, const struct cred *);
 extern long prune_icache_sb(struct super_block *sb, struct shrink_control *sc);
 extern void inode_add_lru(struct inode *inode);
 extern int dentry_needs_remove_privs(struct dentry *dentry);
+void lock_two_inodes(struct inode *inode1, struct inode *inode2,
+		     unsigned subclass1, unsigned subclass2);
 
 extern bool __atime_needs_update(const struct path *, struct inode *, bool);
 static inline bool atime_needs_update_rcu(const struct path *path,
