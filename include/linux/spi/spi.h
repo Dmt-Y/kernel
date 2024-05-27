69543a9dc83ef9dbf48caa3589153e37d81bc6b5
Takashi Iwai <tiwai@suse.de>
2024-05-27 17:08:09 +0200

    kABI workaround for spi_controller (CVE-2021-47469 bsc#1225347).
    suse-commit: af00c9bf9ac7c0230fa9599527aee9447473e08d

--- include/linux/spi/spi.h
+++ include/linux/spi/spi.h
@@ -471,9 +471,6 @@ struct spi_controller {
 	/* I/O mutex */
 	struct mutex		io_mutex;
 
-	/* Used to avoid adding the same CS twice */
-	struct mutex		add_lock;
-
 	/* lock and mutex for SPI bus locking */
 	spinlock_t		bus_lock_spinlock;
 	struct mutex		bus_lock_mutex;
@@ -591,6 +588,11 @@ struct spi_controller {
 	void			*dummy_tx;
 
 	int (*fw_translate_cs)(struct spi_controller *ctlr, unsigned cs);
+
+#ifndef __GENKSYMS__
+	/* Used to avoid adding the same CS twice */
+	struct mutex		add_lock;
+#endif
 };
 
 static inline void *spi_controller_get_devdata(struct spi_controller *ctlr)
